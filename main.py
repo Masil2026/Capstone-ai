250654e769da4c97b5171550fece9eb6eaf9b46d
ChaeHyunLim <chaehyun010104@gmail.com>
2026-05-04 20:31:27 +0900

    setting(#13): cors 설정 삭제

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.controller.TestController import router as test_router
 
 app = FastAPI(
@@ -10,27 +9,11 @@ app = FastAPI(
     redoc_url="/redoc",
 )
 
-# 허용할 Origin(출처) 리스트
-# origins = [
-#     "http://localhost:8080",    # 로컬 Spring Boot 서버
-#     "http://127.0.0.1:8080",    # 로컬 IP 주소
-# ]
-
-# CORS 미들웨어 등록
-app.add_middleware(
-    CORSMiddleware,
-    # allow_origins=origins,            # 허용할 도메인 목록
-    allow_origins=["*"],              # 모든 도메인 허용
-    allow_credentials=True,           # 쿠키/인증 정보 포함 허용 여부
-    allow_methods=["*"],              # 모든 HTTP 메서드 허용 (GET, POST, PUT 등)
-    allow_headers=["*"],              # 모든 HTTP 헤더 허용
-)
-
 app.include_router(test_router, prefix="/api/test", tags=["Test"])
 
 @app.get("/", tags=["Health Check"])
 async def root():
-    return {"message": "Capstone AI API is running(CORS Settings Applied!)"}
+    return {"message": "Capstone AI API is running"}
 
 if __name__ == "__main__":
     import uvicorn
